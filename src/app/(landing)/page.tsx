e8e8a6a4a82b002de682df2f61d3bde94ddfa930
afnansiddiqui__ <afnan.sid0@gmail.com>
2024-01-27 20:05:00 +0500

    checkrerer

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -5,7 +5,7 @@ const LandingPage = () => {
     return (
         <>
             <div>
-                Landing Page (Unprotected) hehehdasdsa
+                Landing Page (Unprotected) learning git
             </div>
             <div>
                 <Link href="/sign-in">
