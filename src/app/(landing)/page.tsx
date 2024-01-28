487c3f142b240a227de6c4f226f25e62effa0342
Afnan Siddiqui <afnan.sid0@gmail.com>
2024-01-28 19:04:38 +0500

    From Muhammad Afnan

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -5,7 +5,7 @@ const LandingPage = () => {
     return (
         <>
             <div>
-                Landing Page (Unprotected) from Afnan and Umar ali
+                Landing Page (Unprotected) from Muhammad Afnan and Umar ali
             </div>
             <div>
                 <Link href="/sign-in">
