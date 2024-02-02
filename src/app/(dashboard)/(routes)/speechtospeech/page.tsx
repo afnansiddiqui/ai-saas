1ceaf428ae6ac2761b1ab505a26e2146c73e1375
SyedMustafaqadri <linkmustafa75@gmail.com>
2024-02-03 01:49:22 +0500

    Build: Set the ui of Speech to Speech

--- src/app/(dashboard)/(routes)/speechtospeech/page.tsx
+++ src/app/(dashboard)/(routes)/speechtospeech/page.tsx
@@ -2,6 +2,8 @@
 import 'regenerator-runtime/runtime';
 import React, { useEffect, useState } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import { UserAvatar } from '@/components/user-avatar';
+import { BotAvatar } from '@/components/bot-avatar';
 
 export default function App() {
   const { listening, transcript } = useSpeechRecognition();
@@ -53,24 +55,44 @@ export default function App() {
   },[transcript,listening] )
 
   return (
-    <>
-      {listening ? (
-        <p>Go Ahead I&apos;m Listening......</p>
-      ) : (
-        <p>Click the button and ask me anything</p>
-      )}
-      <button
-        onClick={() => {
-          SpeechRecognition.startListening();
-        }}
-        className="bg-blue-500 text-white px-6 py-3 rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue active:bg-blue-800"
-      >
-        Ask me anything
-      </button>
-
-      {transcript && <div>{transcript}</div>}
-      {thinking && <div>Thinking...</div>}
-      {aiText && <div>{aiText}</div>}
-    </>
-  );
+		<>
+			<div className="flex flex-col items-center justify-center">
+				<div className="flex flex-col w-[100vw] h-[75vh] p-10 gap-5">
+					{transcript && (
+						<div className="flex gap-5">
+							<UserAvatar />
+							<p>{transcript}</p>
+						</div>
+					)}
+					{thinking && (
+						<div className="flex gap-5">
+							<BotAvatar />
+							<p>Thinking...</p>
+						</div>
+					)}
+					{aiText && (
+						<div className="flex gap-5">
+							<BotAvatar />
+							<p>{aiText}</p>
+						</div>
+					)}
+				</div>
+				<div className="flex flex-col items-center justify-center">
+					{listening ? (
+						<p>Go Ahead I&apos;m Listening......</p>
+					) : (
+						<p>Click the button and ask me anything</p>
+					)}
+					<button
+						onClick={() => {
+							SpeechRecognition.startListening();
+						}}
+						className="bg-blue-500 text-white px-6 py-3 rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue active:bg-blue-800"
+					>
+						Ask me anything
+					</button>
+				</div>
+			</div>
+		</>
+	);
 }
